5f5bb8d5fc356df117dc9380d7436be91f20a523
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-07-03 11:34:59 -0600

    Delete validations for email and need name uniqueness to work with seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
   validates :password, presence: true
   validate :password_correct?, on: :update
 
