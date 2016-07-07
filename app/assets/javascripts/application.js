55b28431a68fea15c366f98824e0fa32e3439c22
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-07-07 09:12:56 -0600

    Re add auto-rotating Begin styling for user dashboard and login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,13 +9,13 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
-//= require bootstrap.min
-// $(document).ready(function() {
-//     $('.carousel').carousel({
-//       interval: 2400;
-//     })
-//   });
+
+// require jquery
+// require jquery_ujs
+// require_tree .
+// require bootstrap.min
+$(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 2400;
+    })
+  });
