dd631398d621eccecfa6aa2c7c16e1fc31be3898
Kerry <kerry.sheldon@gmail.com>
2016-07-07 11:22:11 -0600

    hardcoded inline working

--- app/presenters/analytics_presenter.rb
+++ app/presenters/analytics_presenter.rb
@@ -1,7 +1,7 @@
 class AnalyticsPresenter
 
   def self.get_data
-    {"Merchant" => {items: 1, customers: 12, invoices: 3, average_price: 4}, "All" => {items: 4, customers: 11, invoices: 1, average_price: 1}}
+    {"Merchant" => {items: 1, customers: 112, invoices: 3, average_price: 4}, "All" => {items: 4, customers: 200, invoices: 1, average_price: 1}}
   end
 
 
