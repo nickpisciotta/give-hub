9010ab81bfdcbb6bb1566b38d948b282259bcaaf
Kerry <kerry.sheldon@gmail.com>
2016-07-07 15:29:11 -0600

    model tests complete

--- app/models/needs_category.rb
+++ app/models/needs_category.rb
@@ -1,9 +1,10 @@
 class NeedsCategory < ActiveRecord::Base
   validates :name, presence: true
   has_many :needs
-  before_create :create_slug
   has_many :need_items, through: :needs
   has_many :recipients, through: :need_items
+
+  before_create :create_slug
 
   def create_slug
     self.slug = self.name.parameterize
